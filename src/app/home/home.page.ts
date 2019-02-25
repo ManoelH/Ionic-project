76fb4ede1fcc666a051b56895a67baad4504cf02
manoel <henriqe115@gmail.com>
2019-02-25 16:58:11 -0300

    valifdação do login sem banco de dados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  user = {
+    email:"",
+    senha:""
+  }
+  mensagem;
 
+  login() {
+    if(this.user.email === 'teste@teste.com' && this.user.senha === '123asd'){
+      this.mensagem = '';
+    }
+    else if(this.user.email != '' && this.user.senha != ''){
+      this.mensagem = 'EMAIL OU SENHA ESTÃO INCORRETOS!';
+    }
+  }
+  ngOnInit() {
+    
+  }
 }
