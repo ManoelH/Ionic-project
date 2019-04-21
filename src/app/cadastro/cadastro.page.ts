c9ae4b10976e338241c1a7721414b1d45888aa50
manoelh <henriqe115@gmail.com>
2019-04-21 12:30:40 -0300

    validação dos campos da tela de cadastro com form

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MenuController, ToastController } from '@ionic/angular';
+import { userInfo } from 'os';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,8 +11,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.page.scss'],
 })
 export class CadastroPage implements OnInit {
+  user = {
+    email:"",
+    senha:"",
+    senha2:"",
+    telefone:""
+  }
+  
+  formulario: FormGroup; 
+  
+  constructor(private formBuilder: FormBuilder, private rounter: Router, private toast:ToastController) {
+    this.formulario = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha:['', [Validators.required, Validators.minLength(6)]],
+      senha2:['', [Validators.required, Validators.minLength(6)]],
+      telefone:['',[Validators.required]]
+    });
+}
 
-  constructor() { }
+async presentToast() {
+  const toast = await this.toast.create({
+    message: 'ERRO! AS SENHAS NÃO COINCIDEM',
+    duration: 2000,
+    position: 'top',
+    animated: true,
+    color: 'danger'
+  });
+  toast.present();
+}
+
+  cadastrar(){
+    if(this.user.senha === this.user.senha2){
+      console.log("CADASTROU");
+    }
+    else{
+      this.presentToast();
+    }
+}
+
 
   ngOnInit() {
   }
